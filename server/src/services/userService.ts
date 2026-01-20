84420956d534228e7679c9e0ed5a360b357822c3
Sanskar Kotalwar <kotalwarsanskar@gmail.com>
2026-01-20 22:52:20 +0530

    Added documentation header to userService

--- server/src/services/userService.ts
+++ server/src/services/userService.ts
@@ -1,3 +1,9 @@
+/**
+ * User Service
+ * Handles all user profile operations and saved schemes
+ * Author: Sanskar Kotalwar
+ */
+
 import db from '../config/database.js';
 import { v4 as uuidv4 } from 'uuid';
 import type {
